03ab79e6ef5eb436707272aaa4e2deceea32d307
shaikh shahzade <shahzade10@gmail.com>
2026-03-11 01:31:03 +0530

    first commit

---
+++ accounts/src/main/java/com/demo/accounts/Controller/AccountsController.java
@@ -0,0 +1,13 @@
+package com.demo.accounts.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class AccountsController {
+    
+    
+    
+}
