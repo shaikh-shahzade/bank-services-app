89474a5e82e579e49112b91a41083fa31b0242b8
shaikh shahzade <shahzade10@gmail.com>
2026-03-11 04:56:04 +0530

    Added swagger Definitions

--- accounts/src/main/java/com/demo/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/demo/accounts/AccountsApplication.java
@@ -4,8 +4,34 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Shaikh Shahzade",
+						email = "shahzade10@gmail.com",
+						url = "https://www.demo.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://github.com/shaikh-shahzade"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "Accounts microservice REST API Documentation",
+				url = "https://github.com/shaikh-shahzade"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
