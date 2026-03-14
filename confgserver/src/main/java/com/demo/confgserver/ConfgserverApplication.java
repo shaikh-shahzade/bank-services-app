f17563181393e15c487e7d8475b5a6cc58fd11f1
shaikh shahzade <shahzade10@gmail.com>
2026-03-15 02:36:50 +0530

    Create Config server service

---
+++ confgserver/src/main/java/com/demo/confgserver/ConfgserverApplication.java
@@ -0,0 +1,13 @@
+package com.demo.confgserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfgserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfgserverApplication.class, args);
+	}
+
+}
---
+++ confgserver/src/test/java/com/demo/confgserver/ConfgserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.confgserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfgserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
