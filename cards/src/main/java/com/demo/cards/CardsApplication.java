a7d73110f3e0fc59c1c0b21126e666c9bfab55bf
shaikh shahzade <shahzade10@gmail.com>
2026-04-12 23:35:25 +0530

    Initialize Cards microservice

---
+++ cards/src/main/java/com/demo/cards/CardsApplication.java
@@ -0,0 +1,42 @@
+package com.demo.cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.demo.cards.controller") })
+@EnableJpaRepositories("com.demo.cards.repository")
+@EntityScan("com.demo.cards.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Shaikh Shahzade",
+						email = "Shahzade@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Cards microservice REST API Documentation",
+				url = "https://www.demo.com/swagger-ui.html"
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+}
+
