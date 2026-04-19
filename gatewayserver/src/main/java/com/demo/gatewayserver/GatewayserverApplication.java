570dbe5ce2ff778df3c1b0e591d63a6fe6806399
shaikh shahzade <shahzade10@gmail.com>
2026-04-20 02:25:34 +0530

    Addded sucurity with KeyCloak in gatewayserver

--- gatewayserver/src/main/java/com/demo/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/demo/gatewayserver/GatewayserverApplication.java
@@ -1,5 +1,6 @@
 package com.demo.gatewayserver;
 
+import com.demo.gatewayserver.controller.FallbackController;
 import java.time.LocalDateTime;
 
 import org.springframework.boot.SpringApplication;
@@ -11,7 +12,13 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class GatewayserverApplication {
 
-	public static void main(String[] args) {
+	private final FallbackController fallbackController;
+
+    GatewayserverApplication(FallbackController fallbackController) {
+        this.fallbackController = fallbackController;
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
@@ -21,7 +28,10 @@ public class GatewayserverApplication {
 				.route(p -> p
 						.path("/shah/accounts/**")
 						.filters(f -> f.rewritePath("/shah/accounts/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+								.setFallbackUri("forward:/contactSupport"))
+							)
 						.uri("lb://ACCOUNTS"))
 				.route(p -> p
 						.path("/shah/loans/**")
